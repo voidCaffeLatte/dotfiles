d1e2e3be241742db0d02fdc652c0d2ddff1790c4
voidCaffeLatte <void.caffelatte@gmail.com>
2026-01-25 19:14:57 +0900

    Optimize tool initialization in config.fish

--- config.fish
+++ config.fish
@@ -1,11 +1,15 @@
-if type -q brew
-    eval (brew shellenv)
+if type -q /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
 end
 
-starship init fish | source
+if type -q starship
+    starship init fish | source
+end
+
+# Activate mise if installed locally
+# (Homebrew installations are automatically activated via MISE_FISH_AUTO_ACTIVATE)
+if type -q ~/.local/bin/mise
+    ~/.local/bin/mise activate fish | source
+end
 
 abbr --add -- g git
-
-if type -q mise
-    mise activate fish | source
-end
