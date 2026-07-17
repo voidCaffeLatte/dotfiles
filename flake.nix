ed476be169b9bcd81bdd5ac89c79538f7f6ee757
voidCaffeLatte <76644514+voidCaffeLatte@users.noreply.github.com>
2026-07-18 01:47:48 +0900

    Add Home Manager configuration

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.${builtins.currentSystem};
+    in
+    {
+      homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+      };
+    };
+}
